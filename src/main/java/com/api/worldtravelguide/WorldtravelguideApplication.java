f9e8517f80a549fd87fe4f5fc4976bf7ced5074e
Erman ÖZ <bungleon_@hotmail.com>
2018-10-01 11:54:29 +0300

    message

---
+++ src/main/java/com/api/worldtravelguide/WorldtravelguideApplication.java
@@ -0,0 +1,12 @@
+package com.api.worldtravelguide;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WorldtravelguideApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WorldtravelguideApplication.class, args);
+    }
+}
